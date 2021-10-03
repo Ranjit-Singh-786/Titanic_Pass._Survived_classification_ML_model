c08a35b1ec42b3f5fd11dc56bd249bbef6975e76
Ranjit-Singh-786 <jiradhey402@gmail.com>
2021-10-03 20:29:59 +0530

    finally project completed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import joblib
 from flask import Flask,render_template,request
+from flask_cors import cross_origin
 import sklearn
 import pandas
 import numpy
@@ -9,52 +10,35 @@ from sklearn.preprocessing import StandardScaler
 
 app=Flask(__name__)
 model=joblib.load('titanic_model_save_with_84%_accuracy')
-@app.route('/',methods=['GET'])
+@app.route('/')            #,methods=['GET']
+@cross_origin()
 def Home():
     return render_template('index.html')
-standard_to = StandardScaler()
+# standard_to = StandardScaler()
 
-@app.route("/predict",methods=['POST'])           #
+@app.route("/predict",methods=["GET","POST"])           #,methods=['POST']
+@cross_origin()
 def predict():
-
-    
     if request.method == 'POST':
-    pclass=int(request.form['pclass'])
-    age_of_passenger=float(request.form['age'])
-    sibsp=int(request.form['sibsp'])
-    parch=int(request.form['parch'])
-    female=int(request.form['sex_female'])
-    male=int(request.form['sex_male'])
-    embark_C=int(request.form['embarked_c'])
-    embark_Q=int(request.form['embarked_q'])
-    embark_S=int(request.form['embarked_s'])
+        pclass=int(request.form['pclass'])
+        age_of_passenger=float(request.form['age'])
+        sibsp=int(request.form['sibsp'])
+        parch=int(request.form['parch'])
+        female=int(request.form['sex_female'])
+        male=int(request.form['sex_male'])
+        embark_C=int(request.form['embarked_c'])
+        embark_Q=int(request.form['embarked_q'])
+        embark_S=int(request.form['embarked_s'])
 
-    town_c=int(request.form['embark_town_Cherbourg'])
-    town_q=int(request.form['embark_town_Queenstown'])
-    town_s=int(request.form['embark_town_Southampton'])
-    prediction=model.predict([[pclass,age_of_passenger,sibsp,parch,female,male,embark_C,embark_Q,embark_S,town_c,town_q,town_s]])
-    output=round(prediction[0],2)
-    return render_template('index.html',prediction_texts=str(output))
-    return output
-    if output <1:
-        return render_template('index.html',prediction_texts="Sorry , he has no more")
-    else:
-        return render_template('index.html',prediction_text='this person is servived{}'.format(output))
-
-
-            
-    else:
-        return render_template('index.html')
-# ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-    
-    
-    
-
-
-# for_chek_record=[pclass,age_of_passenger,sibsp,parch,female,male,embark_C,embark_Q,embark_S,town_c,town_q,town_s]
-# print(for_chek_record)
-
-
+        town_c=int(request.form['embark_town_Cherbourg'])
+        town_q=int(request.form['embark_town_Queenstown'])
+        town_s=int(request.form['embark_town_Southampton'])
+        prediction=model.predict([[pclass,age_of_passenger,sibsp,parch,female,male,embark_C,embark_Q,embark_S,town_c,town_q,town_s]])
+        output=round(prediction[0],2)
+        if output <1:
+            return render_template('death.html',prediction_text=str(output))        
+        else:
+            return render_template('survived.html')
 
 if __name__ =='__main__':
     app.run(debug=True)
